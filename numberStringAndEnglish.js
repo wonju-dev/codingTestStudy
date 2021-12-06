c0184bff8698d0f7984fd146bb75266eb566b0f3
WonjuLee <wonjulee.dev@gmail.com>
2021-12-06 23:45:24 +0900

    Update numberStringAndEnglish.js

--- numberStringAndEnglish.js
+++ numberStringAndEnglish.js
@@ -1,4 +1,3 @@
-const NUMBERS = ['0','1','2','3','4','5','6','7','8','9'];
 const MAP = {
     zero : '0',
     one : '1',
@@ -14,16 +13,16 @@ const MAP = {
 
 const getKey = (array) => {
     if (array.length === 0) return false;
+    
     const key = array.reduce((a,b)=> a+=b); 
     if (MAP[key] === undefined) return false;
     else return key;
 }
 
-
-
 let strings = [];
 function solution(s) {
     let answer = "";
+    const NUMBERS = Object.values(MAP)
     for (let i = 0 ; i < s.length ; i++){
         if (NUMBERS.includes(s[i])) answer += s[i];
         else strings.push(s[i]);
