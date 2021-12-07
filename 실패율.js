fae69664d63094f7abca8422bf9db3b489e833e0
WonjuLee <wonjulee.dev@gmail.com>
2021-12-08 00:34:07 +0900

    실패율 실패2 (70/100)

--- 실패율.js
+++ 실패율.js
@@ -1,25 +1,20 @@
 function solution(N, stages) {
-    const top = new Array(N+1);
-    const bottom = new Array(N+1);
-    for (let i = 1 ; i <= N ; i++){
-        top[i] = 0;
-        bottom[i] = 0;
+    const challengeMap = {}
+    stages.forEach((stage) => !challengeMap[stage] ? challengeMap[stage] = 1 : challengeMap[stage] += 1);
+    
+    const failureRate = [];
+    let numOfStagePeople = stages.length;
+    for (let i = 0 ; i < N; i ++){
+        let numOfPeople = challengeMap[`${i+1}`] ? challengeMap[`${i+1}`] : 0;
+        failureRate[i] = numOfPeople/ numOfStagePeople
+        numOfStagePeople -= numOfPeople;
     }
-    stages.forEach((stage)=>{
-        for (let i = 1 ; i <= N ; i ++) {
-            if (stage >= i) bottom[i] += 1;
-            if (stage === i) top[i] += 1;
-        }
-    })
-    let failure = bottom.map((bot,index)=>{
-        if (index !== 0) return Number(top[index] / bot)
-    })
-    failure = failure.slice(1,N+1);
+    
     const answer = [];
-    for (let i = 0 ; i < N ; i++){
-        const maxIndex = getMaxIndex(failure);
+    for (let i = 0 ; i < N ; i ++){
+        const maxIndex = getMaxIndex(failureRate);
         answer.push(Number(maxIndex+1));
-        failure.splice(maxIndex,1);
+        failureRate.splice(maxIndex,1,-1);
     }
     return answer;
 }
