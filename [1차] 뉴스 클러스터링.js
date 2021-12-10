242837bceb6964e547b68ec88ce2ca9372e372a9
WonjuLee <wonjulee.dev@gmail.com>
2021-12-11 01:54:33 +0900

    [1차] 뉴스 클러스터링

--- [1차] 뉴스 클러스터링.js
+++ [1차] 뉴스 클러스터링.js
@@ -1,5 +1,5 @@
 function solution(str1, str2) {
-    const regex = /(?<string>[\w]*)/;
+    const regex = /([A-Z]{2})/;
     const upperStr1 = str1.toUpperCase();
     const upperStr2 = str2.toUpperCase();
     const slicedStrings = {
@@ -9,32 +9,35 @@ function solution(str1, str2) {
     [upperStr1, upperStr2].forEach((upperString, idx)=>{
         for (let i = 0 ; i < upperString.length -1 ; i ++) {
             const slicedString = upperString.slice(i,i+2);
-            const string = regex.exec(slicedString).groups['string']; 
+            const string = regex.exec(slicedString);
             if (string) {
-                if (idx === 0) slicedStrings.string1.push(string);
-                else slicedStrings.string2.push(string);
+                if (idx === 0) slicedStrings.string1.push(string[0]);
+                else slicedStrings.string2.push(string[0]);
             } 
         }
     })
     
-    const s1s = slicedStrings.string1.slice();
-    const s2s = slicedStrings.string2.slice();
-    
-    const Sum = s1;
-    s1.forEach((string)=>{
-        const index = s2.findIndex((string2)=>string2===string);
-        if (index !== -1) s2.splice(index,1);
+    const copyString1 = slicedStrings.string1.slice();
+    const copyString2 = slicedStrings.string2.slice();
+    // 교집합
+    const intersection = [];
+    slicedStrings.string1.forEach((chunk1, string1Index) => {
+        const string2Index = slicedStrings.string2.findIndex((chunk2) => chunk2 === chunk1);
+        if (string2Index !== -1) {
+            intersection.push(chunk1);
+            slicedStrings.string1.splice(string1Index,1,-1);
+            slicedStrings.string2.splice(string2Index,1,-1);
+        }
     })
-    const newSum = Sum.concat(s2);
-    
-    const common = [];
-    s1s.forEach((string, index)=>{
-        const s2Index = s2s.findIndex((s2String)=> s2String === string);
-        if (s2Index !== -1) {
-            common.push(s1s[index]);
-            s1.splice(index,1,-1);
-            s2s.splice(s2Index,1,-1);
-        } 
+    // 합집합
+    let union = [].concat(copyString1);
+    copyString1.forEach((chunk1, string1Index) => {
+        const string2Index = copyString2.findIndex((chunk2) => chunk2 === chunk1);
+        if (string2Index !== -1){
+            copyString2.splice(string2Index,1);
+        }
     })
-    return  common.length / newSum.length * 65536;
+    union = union.concat(copyString2);
+    if (intersection.length === 0 && union.length === 0) return 65536;
+    return Math.floor(intersection.length / union.length * 65536);
 }
