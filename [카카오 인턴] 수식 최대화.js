8ba0ccfa44c9dc9950c6a236199a2b3a25a027d3
WonjuLee <wonjulee.dev@gmail.com>
2021-12-12 23:37:27 +0900

    [카카오 인턴] 수식 최대화

--- [카카오 인턴] 수식 최대화.js
+++ [카카오 인턴] 수식 최대화.js
@@ -3,22 +3,23 @@ function solution(expression) {
     // 1. 주어진 수식의 숫자, 연산 기호를 분리
     const operators = expression.match(regex);
     const operands = expression.split(regex);
-    // 2. 분리한 연산 기호들의 실행 순서 순열 만듦 (아놔 3개만 고려하면 됐자너~~~~)
+    // 2. 분리한 연산 기호들의 실행 순서 순열 만듦
     const CASESE = [['+','-','*'],['+','*','-'],['-','+','*'],['-','*','+'],['*','-','+'],['*','+','-']]
     // 3. 경우를 하나씩 돌리기
     let biggestNumber = 0;
     CASESE.forEach((operatorCase)=>{
         const copyOperands = operands.slice();
         const copyOperators = operators.slice();
-        operatorCase.forEach((operator)=>{
-            while (copyOperators.includes(operator)) {
-                const index = copyOperators.findIndex((operand)=>operand)
+        operatorCase.forEach((targetOperator)=>{
+            while (copyOperators.includes(targetOperator)) {
+                const index = copyOperators.findIndex((operator)=>operator === targetOperator);
+                copyOperands[index] = eval(`${copyOperands[index]} ${targetOperator} ${copyOperands[index+1]}`);
                 copyOperators.splice(index,1);
-                copyOperands[index+1] = eval(`${copyOperands[index]} ${operator} ${copyOperands[index+1]}`);
+                copyOperands.splice(index+1,1);
             }
-            const number = copyOperands[operatorCase.length-1]
-            if (biggestNumber < number) biggestNumber = number;
         })
+        const number = Math.abs(copyOperands[0]);
+        if (biggestNumber < number) biggestNumber = number;
     })
     return biggestNumber;   
 }
