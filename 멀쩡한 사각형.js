4bdf6db4609fa2d3e3c740c58aab64331bc5aef1
WonjuLee <wonjulee.dev@gmail.com>
2021-12-15 00:52:27 +0900

    멀쩡한 사각형 실패 (94.4 / 100)

--- 멀쩡한 사각형.js
+++ 멀쩡한 사각형.js
@@ -1,4 +1,23 @@
 function solution(w, h) {
+    if (w === 1 || h === 1) return 0;
+    else if (w === h) return w * h - w;
+    
+    const traces = [];
+    for (let i = 0 ; i <= w ; i++) traces.push(getY(i, w, h))
+    
+    let totalBlock = w * h;
+    for (let i = 0 ; i < w ; i++) totalBlock -= (Math.ceil(traces[i]) - Math.floor(traces[i+1]));
+    
+    return totalBlock;
+}
+
+const getY = (x, w, h) => -1 * h / w * x + h;
+
+/*
+
+이전 버전
+W가 홀수인지 짝수인지에 따라 사용불가능한 블록 개수가 다름
+function solution(w, h) {
     // 길이가 1이면 전부 못 씀
     if (w === 1 || h === 1) return 0;
     // 정사각형이면 길이만큼 못 씀
@@ -9,3 +28,4 @@ function solution(w, h) {
 }
 
 const getY = (x, w, h) => -1 * h / w * x + h;
+*/
