4f6026c9818be971d1e849bc09a86505ef0f0381
WonjuLee <wonjulee.dev@gmail.com>
2021-12-30 00:08:35 +0900

    Update 실패율.js

--- 실패율.js
+++ 실패율.js
@@ -1,39 +1,44 @@
-// 이전에는 아무도 도착하지 못한 Stage를 고려하지 않았다
-
-const init = (N) => {
-    const mother = new Array(N);
+const getChallengingParticipants = (N, stages) => {
     const child = new Array(N);
-    for (let i = 0 ; i < N ; i++){
-        mother[i] = 0;
-        child[i] = 0;
-    }
-    return [mother, child];
+    for (let i = 0 ; i < N ; i++) child[i] = 0;
+    stages.forEach((stage)=> stage <= N ? child[stage-1] += 1 : "");
+    return child;
 }
 
-function solution(N, stages) {
-    const [mother, child] = init(N);    
-    
-    stages.forEach((stage)=>{
-        if (stage > N) for (let i = 0; i < stage-1 ; i++) mother[i] += 1;
-        else {
-            for (let i = 0; i < stage ; i++) mother[i] += 1;
-            child[stage-1] +=1;
-        }
+const getFinishParticipants = (child, NUMBER_OF_PARTICIPANTS) => {
+    return child.map((number,index)=>{
+        let beforeNumber = 0;
+        for (let i = 0 ; i < index ; i++) beforeNumber += child[i];
+        return NUMBER_OF_PARTICIPANTS-beforeNumber;
     })
-    
-    const rate = mother.map((bot, index) => {
+}
+
+const calculateFinishRate = (mother, child) => {
+    return mother.map((bot, index) => {
         if (child[index] !== 0) return child[index] / bot;
         else return 0;
     });
+}
 
+const getSortedRating = (rate, N) => {
     const answer = [];
+    
     for (let i = 0 ; i < N ; i++) {
         let maxIndex = 0;
-        for (let i = 1 ; i < rate.length ; i++){
-            if (rate[i] > rate[maxIndex]) maxIndex = i; 
-        }
+        for (let i = 1 ; i < rate.length ; i++) rate[i] > rate[maxIndex] ? maxIndex = i : ""; 
         answer.push(maxIndex+1);
         rate[maxIndex] = -1;
     }
+    return answer;
+}
+
+function solution(N, stages) {
+    const NUMBER_OF_PARTICIPANTS = stages.length;
+    
+    const child = getChallengingParticipants(N, stages);
+    const mother = getFinishParticipants(child, NUMBER_OF_PARTICIPANTS);
+    const rate = calculateFinishRate(mother, child);
+
+    const answer = getSortedRating(rate, N);
     return answer;
 }
