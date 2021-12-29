ac855f9229d37806d6f8e0c97495e1b9057bf2fd
WonjuLee <wonjulee.dev@gmail.com>
2021-12-29 23:51:06 +0900

    Update 실패율.js

--- 실패율.js
+++ 실패율.js
@@ -1,3 +1,5 @@
+// 이전에는 아무도 도착하지 못한 Stage를 고려하지 않았다
+
 const init = (N) => {
     const mother = new Array(N);
     const child = new Array(N);
@@ -19,10 +21,13 @@ function solution(N, stages) {
         }
     })
     
-    const rate = mother.map((bot, index) => child[index] / bot);
+    const rate = mother.map((bot, index) => {
+        if (child[index] !== 0) return child[index] / bot;
+        else return 0;
+    });
 
     const answer = [];
-    while (rate.reduce((a,b)=>a+b) !== -1 * N) {
+    for (let i = 0 ; i < N ; i++) {
         let maxIndex = 0;
         for (let i = 1 ; i < rate.length ; i++){
             if (rate[i] > rate[maxIndex]) maxIndex = i; 
@@ -30,41 +35,5 @@ function solution(N, stages) {
         answer.push(maxIndex+1);
         rate[maxIndex] = -1;
     }
-    
     return answer;
 }
-
-/*
-function solution(N, stages) {
-    const challengeMap = {}
-    stages.forEach((stage) => !challengeMap[stage] ? challengeMap[stage] = 1 : challengeMap[stage] += 1);
-    
-    const failureRate = [];
-    let numOfStagePeople = stages.length;
-    for (let i = 0 ; i < N; i ++){
-        let numOfPeople = challengeMap[`${i+1}`] ? challengeMap[`${i+1}`] : 0;
-        failureRate[i] = numOfPeople/ numOfStagePeople
-        numOfStagePeople -= numOfPeople;
-    }
-    
-    const answer = [];
-    for (let i = 0 ; i < N ; i ++){
-        const maxIndex = getMaxIndex(failureRate);
-        answer.push(Number(maxIndex+1));
-        failureRate.splice(maxIndex,1,-1);
-    }
-    return answer;
-}
-
-const getMaxIndex = (array) => {
-    let maxIndex = 0 ;
-    let maxValue = array[0];
-    for (let i = 1 ; i < array.length ; i++){
-        if (maxValue < array[i]){
-            maxIndex = i;
-            maxValue = array[i];
-        }
-    }
-    return maxIndex;
-}
-*/
