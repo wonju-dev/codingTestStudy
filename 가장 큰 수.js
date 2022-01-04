7c098b29de856e778d9fae7322c0051288c83924
WonjuLee <wonjulee.dev@gmail.com>
2022-01-04 23:34:41 +0900

    [220104] 가장 큰 수 실패 60/100

--- 가장 큰 수.js
+++ 가장 큰 수.js
@@ -1,61 +1,25 @@
 function solution(numbers) {
-    let answer = "";
-    let flag = new Array(numbers.length);
-    for (let i = 0 ; i < flag.length ; i++) flag[i] = false;
-    while (flag.includes(false)) {
-        const biggestArray = findBiggest(numbers);
-        if (biggestArray.length === 1) {
-            const index = numbers.findIndex((number)=>number === biggestArray[0])
-            numbers[index] = -1;
-            flag[index] = true;
-            answer += String(biggestArray[0]);
+    const fourLength = numbers.map((number)=>{
+        const numberLength = String(number).length; 
+        if (numberLength < 4) {
+            let stringedNumber = String(number);
+            for (let i = numberLength; i < 4 ; i++) stringedNumber += stringedNumber[numberLength-1]; 
+            return stringedNumber;
         }
-        else {
-            const biggest = compareNextDigit(biggestArray);
-            const index = numbers.findIndex((number)=> number === Number(biggest));
-            numbers[index] = -1;
-            flag[index] = true;
-            answer += String(biggest);
-        }
-    }
-    return answer;
-}
-
-const compareNextDigit = (array) => {
-    let longest = String(array[0]).length;
-    for (let i = 1 ; i < array.length ; i++) {
-        const nextLength = String(array[i]).length; 
-        if (nextLength > longest) longest = nextLength;
-    }
-    const processedArray = array.map((number)=>{
-        let stringNumber = String(number);
-        if (String(number).length < longest) {
-            let lastNumber = stringNumber[stringNumber.length-1];
-            while (stringNumber.length !== longest) {
-                stringNumber += lastNumber;
-            }
-        }
-        return Number(stringNumber);
     })
-    let biggestIndex = 0;
-    for (let i = 0 ; i < processedArray.length ; i++) {
-        if (processedArray[biggestIndex] < processedArray[i]) biggestIndex = i;
+    let answer = "";
+    while (fourLength.length !==0) {
+        const biggestIndex = getBiggestIndex(fourLength);
+        answer += String(numbers[biggestIndex]);
+        numbers.splice(biggestIndex,1);
+        fourLength.splice(biggestIndex,1);
     }
-    return array[biggestIndex];
+    while (answer[0] === '0') answer = answer.slice(1);
+    return answer === "" ? '0' : answer;
 }
 
-const findBiggest = (numbers) => {
-    let answer = [numbers[0]];
-    const firstNumbers = numbers.map((number)=> number < 0 ? 0 : Number(String(number)[0]));
-    let biggest = firstNumbers[0];
-    for (let i = 1; i < firstNumbers.length ; i++) {
-        if (biggest < firstNumbers[i]) {
-            answer = []
-            biggest = firstNumbers[i];
-            answer.push(numbers[i]);
-        } else if (biggest === firstNumbers[i]) {
-            answer.push(numbers[i]);
-        }
-    }
-    return answer;
+const getBiggestIndex = (array) => {
+    let biggestIndex = 0;
+    for (let i = 1; i < array.length ; i++) if (array[biggestIndex] < array[i]) biggestIndex = i;
+    return biggestIndex;
 }
