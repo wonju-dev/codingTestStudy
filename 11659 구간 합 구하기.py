aed04d50e779ec06f43deccf354442b01166c77d
wonju-dev <wonjulee.dev@gmail.com>
2022-07-14 12:37:30 +0900

    [220714] 11659 success

--- 11659 구간 합 구하기.py
+++ 11659 구간 합 구하기.py
@@ -1,9 +1,15 @@
-n, m = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
 numbers = list(map(int, input().split()))
 
+sum = 0
+results = [0]
+for num in numbers:
+    sum += num
+    results.append(sum)
+
 for _ in range(m):
-    start, end = list(map(int, input().split()))
-    answer = 0
-    for i in range(start - 1, end):
-        answer += numbers[i]
-    print(answer)
+    start, end = map(int, input().split())
+    print(results[end] - results[start - 1])
