3f8e1743f9051b75dca54aca36e691c6ed587cea
wonju-dev <wonjulee.dev@gmail.com>
2022-07-15 16:24:12 +0900

    [220715]9019 DSLR 시간초과

--- 9019 DSLR.py
+++ 9019 DSLR.py
@@ -1,40 +1,35 @@
 from collections import deque
+import sys
 
-def trimZero(num):
-    result = str(num).lstrip("0")
-    return result if result != "" else "0"
+input = sys.stdin.readline
 
 def D(num):
-    return str((int(num) * 2) % 10000)
+    return (2*num)%10000
 
 def S(num):
-    result = str(int(num) - 1)
-    return result if int(result) > 0 else '9999'
+    return (num-1)%10000
 
 def L(num):
-    if len(num) != 4:
-        num = "0" * (4 - len(num)) + num
-    return num[1:4] + num[0]
+    return (10*num+(num//1000))%10000
 
 def R(num):
-    if len(num) != 4:
-        num = "0" * (4 - len(num)) + num
-    return num[3] + num[0:3]
+    return (num//10+(num%10)*1000)%10000
 
 
 case = int(input())
 for _ in range(case):
-    a, b = input().split()
-    numberAndWord = [False for _ in range(10001)]
+    a, b = map(int, input().split())
+    
     queue = deque()
     queue.append((a, ""))
+
+    visited = [False] * 10000
     
-    while trimZero(queue[0][0]) != b:
+    while queue[0][0] != b:
         curr, hist = queue.popleft()
-        numberAndWord[int(curr)] = True
+        visited[int(curr)] = True
 
         for nextNum in ((D(curr), hist + 'D'), (S(curr), hist + 'S'), (L(curr), hist + 'L'), (R(curr), hist + 'R')):
-            if 0 <= int(nextNum[0]) <= 9999 and not numberAndWord[int(nextNum[0])]:
-                queue.append((trimZero(nextNum[0]), nextNum[1]))
-        
-    print("".join(queue[0][1]))
+            if not visited[nextNum[0]]:
+                queue.append((nextNum[0], nextNum[1]))
+    print(queue[0][1])
