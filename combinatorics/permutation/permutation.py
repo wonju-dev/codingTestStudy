eb2fa55f84462732878ca8bf5e46e5153839c6de
wonju-dev <wonjulee.dev@gmail.com>
2022-08-13 18:11:19 +0900

    save

--- combinatorics/permutation/permutation.py
+++
@@ -1,6 +0,0 @@
-from itertools import permutations
-
-numbers = [1,2,3,4,5]
-
-for pair in permutations(numbers, 2):
-    print(pair)
--- search/stringNaive.py
+++
@@ -1,9 +0,0 @@
-string = "'abc'dab'abc'dabe"
-target = "abc"
-
-count = 0
-for i in range(len(string) - len(target)):
-    if string[i:i + len(target)] == target:
-        count += 1
-
-print(count)
