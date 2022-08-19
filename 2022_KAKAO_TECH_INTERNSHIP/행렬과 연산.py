051c3e485a32a54789b97f00de36ee72c62288bc
wonju-dev <wonjulee.dev@gmail.com>
2022-08-20 00:03:31 +0900

    [220820] 행렬과 연산 / 시간초과

--- 2022_KAKAO_TECH_INTERNSHIP/행렬과 연산.py
+++ 2022_KAKAO_TECH_INTERNSHIP/행렬과 연산.py
@@ -7,29 +7,35 @@ def solution(rc, operations):
     for operation in operations:
         if operation == "Rotate":
             last = len(rc) - 1
-            firstRowLast = rc[0][-1]
-            lastRowFirst = rc[last][0]
-
             # first row
-            rc[0] = [rc[1][0]] + rc[0][:-1]
-            
+            firstRow = deque(rc[0])
+            firstRow.appendleft(rc[1][0])
+            firstRowLast = firstRow.pop()
+            rc[0] = list(firstRow)
+            ## print(f"firstRow={rc[0]}")
+
             # last row
-            rc[last] = rc[last][1:] + [rc[last - 1][-1]]
-            
+            lastRow = deque(rc[last])
+            lastRow.append(rc[last - 1][-1])
+            lastRowFirst = lastRow.popleft()
+            rc[last] = list(lastRow)
+            ## print(f"lastRow={rc[last]}")
             # right
-            for i in range(len(rc) - 2, 1, -1):
-                rc[i][-1] = rc[i - 1][-1]
+            for i in range(last - 1, 1, -1):
+                rc[i][-1] = rc[i-1][-1]
             rc[1][-1] = firstRowLast
+            ## print(f"rc[1][-1]={rc[1][-1]}")
             # left
-            for i in range(1, len(rc) - 2):
-                rc[i][0] = rc[i + 1][0]
+            for i in range(1, last - 1):
+                rc[i][0] = rc[i+1][0]
             rc[last - 1][0] = lastRowFirst
+            ## print(f"rc[last - 1][-1]={rc[last - 1][-1]}")
+            # update
         elif operation == "ShiftRow":
-            lastRow = rc.pop()
-            rc.appendleft(lastRow)
+            rc.appendleft(rc.pop())
 
     return list(rc)
 
-# print(solution([[1,2,3],[4,5,6],[7,8,9]], ["Rotate", "ShiftRow"]))
-# print(solution([[8, 6, 3], [3, 3, 7], [8, 4, 9]], ["Rotate", "ShiftRow", "ShiftRow"]))
-# print(solution([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], ["ShiftRow", "Rotate", "ShiftRow", "Rotate"]))
+print(solution([[1,2,3],[4,5,6],[7,8,9]], ["Rotate", "ShiftRow"]))
+print(solution([[8, 6, 3], [3, 3, 7], [8, 4, 9]], ["Rotate", "ShiftRow", "ShiftRow"]))
+print(solution([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], ["ShiftRow", "Rotate", "ShiftRow", "Rotate"]))
