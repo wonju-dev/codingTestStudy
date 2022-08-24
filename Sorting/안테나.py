2443efdb5227d430eba3ff7acc0d7fb4d888465d
wonju-dev <wonjulee.dev@gmail.com>
2022-08-24 23:32:21 +0900

    [220824] 정렬 기출2

--- Sorting/안테나.py
+++ Sorting/안테나.py
@@ -1,16 +1,4 @@
 n = int(input())
 houses = list(map(int, input().split()))
-
-smallest = int(1e9)
-location = 0
-
-for targetHouse in houses:
-    summation = 0
-    for compareHouse in houses:
-        summation += abs(compareHouse - targetHouse)
-
-    if smallest > summation:
-        smallest = summation
-        location = targetHouse
-
-print(location)
+houses.sort()
+print(houses[len(houses) // 2 - 1])
