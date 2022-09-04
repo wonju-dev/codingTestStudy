ddac7d315e542a6167bca64c6c152b683bf9d526
wonju-dev <wonjulee.dev@gmail.com>
2022-09-04 16:03:24 +0900

    save

--- string/1264 모음의 개수.py
+++
@@ -1,9 +0,0 @@
-text = input()
-
-while text != "#":
-    count = 0
-    for i in range(len(text)):
-        if text[i] in ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']:
-            count += 1
-    print(count)
-    text = input()
--- string/25372 성택이의 은밀한 비밀번호.py
+++
@@ -1,5 +0,0 @@
-case = int(input())
-
-for _ in range(case):
-    password = input()
-    print("yes" if 6 <= len(password) <= 9 else "no")
