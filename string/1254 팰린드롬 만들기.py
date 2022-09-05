0c5758089fe9a4686032ef2fb83f7011f0297ff5
wonju-dev <wonjulee.dev@gmail.com>
2022-09-05 17:36:14 +0900

    [220905]1254 팰린드롬 만들기

--- string/1254 팰린드롬 만들기.py
+++ string/1254 팰린드롬 만들기.py
@@ -4,40 +4,25 @@ def isPalindrome(string):
             return False
     return True
 
-def getPartialPalindromStartIndex(string):
+def getPartialPalindrom(string):
     for i in range(len(string) - 1):
         partial = string[i:]
         if isPalindrome(partial):
-            if len(partial) % 2 == 1:
-                return (i + (len(partial) // 2) + 1, True)
-            else:
-                return (i + (len(partial) // 2) - 1, False)
-    return (-1, True) 
+            return partial
+    
+    return None
 
 string = input()
 
 if isPalindrome(string):
     print(len(string))
 else:
-    startIndex, isOdd = getPartialPalindromStartIndex(string)
+    partial = getPartialPalindrom(string)
     
-    if startIndex == -1:
-        startIndex = len(string) - 1
-
-    if not isOdd:
-        string = string[:startIndex + 1] + '_' + string[startIndex + 1:]
-        startIndex += 1
-
-    diff = 1
-
-    while not isPalindrome(string):
-        if startIndex + diff == len(string):
-            string += string[startIndex - diff]
+    if partial != None:
+        print(len(string) + len(string) - len(partial))
+    else:
+        if len(string) % 2 == 0:
+            print(2 * len(string) - 1)
         else:
-            diff += 1
-
-    underbarIndex = string.find("_")
-    if underbarIndex != -1:
-        string = string[:underbarIndex] + string[underbarIndex + 1:]
-
-    print(len(string))
+            print(len(string) + len(string) - 1)
