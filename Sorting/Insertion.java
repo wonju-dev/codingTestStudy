e600360c5ffd7dc53d4771315d026b00d878bffb
wonju-dev <wonjulee.dev@gmail.com>
2022-09-07 23:51:50 +0900

    [220907]insertion sort by java

---
+++ Sorting/Insertion.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Insertion {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int loop = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < loop; i++) {
+            String s = scanner.nextLine();
+            int numOfNumbers = Integer.parseInt(s.substring(0, 1));
+            int[] numberArray = getNumberArray(numOfNumbers, s.substring((2)));
+            insertionSort(numOfNumbers, numberArray);
+        }
+    }
+
+    private static int[] getNumberArray(int size, String substring) {
+        int[] numbers = new int[size];
+        int index = 0;
+
+        for (int i = 0; i < substring.length(); i++) {
+            if (substring.charAt(i) != ' ') {
+                numbers[index] = Character.getNumericValue(substring.charAt(i));
+                index++;
+            }
+        }
+
+        return numbers;
+    }
+
+    private static void insertionSort(int size, int[] numbers) {
+        for (int i = 1; i < size; i++) {
+            int index = i - 1;
+            while (index >= 0) {
+                if (numbers[index + 1] < numbers[index]) {
+                    swap(numbers, index);
+                    index--;
+                } else {
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.println("index " + i + " = " + numbers[i]);
+        }
+    }
+
+    private static void swap(int[] numbers, int index) {
+        int temp = numbers[index + 1];
+        numbers[index + 1] = numbers[index];
+        numbers[index] = temp;
+    }
+}
