773a2368ff0c848eb0f199fd8ae26f71841ea25d
wonju-dev <wonjulee.dev@gmail.com>
2022-09-13 15:27:41 +0900

    [220913]Insertion sort

--- Sorting/insertion/Main.java
+++ Sorting/insertion/Main.java
@@ -1,55 +1,59 @@
 package insertion;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-class Main {
+class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int loop = Integer.parseInt(scanner.nextLine());
+
         for (int i = 0; i < loop; i++) {
-            String s = scanner.nextLine();
-            int numOfNumbers = Integer.parseInt(s.substring(0, 1));
-            int[] numberArray = getNumberArray(numOfNumbers, s.substring((2)));
-            insertionSort(numOfNumbers, numberArray);
+            List<Integer> numberArray = getNumberArray(scanner.nextLine().split(" "));
+            insertionSort(numberArray);
         }
     }
 
-    private static int[] getNumberArray(int size, String substring) {
-        int[] numbers = new int[size];
-        int index = 0;
-
-        for (int i = 0; i < substring.length(); i++) {
-            if (substring.charAt(i) != ' ') {
-                numbers[index] = Character.getNumericValue(substring.charAt(i));
-                index++;
-            }
-        }
-
-        return numbers;
-    }
-
-    private static void insertionSort(int size, int[] numbers) {
+    private static void insertionSort(List<Integer> numberArray) {
         int countCompare = 0;
         int countSwap = 0;
-        for (int i = 1; i < size; i++) {
-            int index = i - 1;
-            while (index >= 0) {
+
+        for (int i = 1; i < numberArray.size(); i++) {
+            int temp = numberArray.get(i);
+            int j = i - 1;
+            while (j >= 0) {
                 countCompare++;
-                if (numbers[index + 1] < numbers[index]) {
+                if (temp < numberArray.get(j)) {
                     countSwap++;
-                    swap(numbers, index);
-                    index--;
+                    numberArray.set(j + 1, numberArray.get(j));
                 } else {
                     break;
                 }
+                j--;
             }
+
+            numberArray.set(j + 1, temp);
+
         }
+
         System.out.println(countCompare + " " + countSwap);
     }
 
-    private static void swap(int[] numbers, int index) {
-        int temp = numbers[index + 1];
-        numbers[index + 1] = numbers[index];
-        numbers[index] = temp;
+    private static List<Integer> getNumberArray(String[] s) {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < s.length; i++) {
+            if (i != 0) {
+                numbers.add(Integer.parseInt(s[i]));
+            }
+        }
+        return numbers;
+    }
+
+    private static void swap(List<Integer> numbers, int j, int i) {
+        int temp = numbers.get(j);
+        numbers.set(j, numbers.get(i));
+        numbers.set(i, temp);
     }
 }
