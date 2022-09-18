e906eaa33323b8365a886672069ec1662464fe58
wonju-dev <wonjulee.dev@gmail.com>
2022-09-18 19:46:12 +0900

    [220918] 택배배송 실패 (시간초과)

--- 220916/실전프로젝트-택배배송.py
+++ 220916/실전프로젝트-택배배송.py
@@ -1,40 +1,62 @@
-INF = int(1e9)
-candis = []
+from collections import deque
 
-def dfs(now, weight, visited):
-    global candis
-
-    if now == b:
-        candis.append(weight)
-        return 
-    else:
-        for next in range(1, n + 1):
-            value = graph[now][next]
-            if not visited[next] and value != INF:
-                cVisited = list(visited)
-                cVisited[next] = True
-                if value < weight:
-                    dfs(next, value, cVisited)
-                else:
-                    dfs(next, weight, cVisited)
+def getIndex(o, d):
+    for i in range(len(graph[o])):
+        next, _ = graph[o][i]
+        if next == d:
+            return i
+    return -1
 
 def initGraph(values):
     o, d, c = values
-    graph[o][d] = c if graph[o][d] == INF else max([graph[o][d], c])
-    graph[d][o] = c if graph[d][o] == INF else max([graph[d][o], c])
 
+    index = getIndex(o, d)
+    if index != -1:
+        if graph[o][index][1] < c:
+            graph[o][index][1] = c
+    else:
+        graph[o].append([d, c])
+
+    index = getIndex(d, o)
+    if index != -1:
+        if graph[d][index][1] < c:
+            graph[d][index][1] = c
+    else:
+        graph[d].append([o, c])        
 
 n = int(input())
 
 for _ in range(n):
     n, m, a, b = map(int, input().split())
-    graph = [[INF] * (n + 1) for __ in range(n + 1)]
-    visited = [True] + [False] * n
-    visited[a] = True
+    graph = [[] for _ in range(n + 1)]
     
     for __ in range(m):
         initGraph(list(map(int, input().split())))
-    
-    dfs(a, INF, visited)    
 
-    print(sorted(candis, reverse=True)[0])
+    q = deque()
+    q.append((a, int(1e9), -1))
+    candis = []
+
+    visited = [False] * 1001
+    count = 0
+    maxi = 0
+
+    while q and count != len(graph[b]):
+        now, weight, before = q.popleft()
+
+        if now == b:
+            if not visited[before]:
+                visited[before] = True
+                count += 1
+                if maxi < weight:
+                    maxi = weight
+        else:
+            for pair in graph[now]:
+                next, wei = pair
+                if next != before:
+                    if weight > wei:
+                        q.append((next, wei, now))
+                    else:
+                        q.append((next, weight, now))
+
+    print(maxi)
