57b28a609f4beae560f8babce482d16f9b6aff82
wonju-dev <wonjulee.dev@gmail.com>
2022-09-26 01:22:38 +0900

    save

--- datastructure/16466 콘서트.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-numbers = sorted(list(map(int, input().split())))
-
-for i in range(1, n):
-    if i != numbers[i - 1]:
-        print(i)
-        break
-else:
-    print(n + 1)
--- math/4153 직각삼각형.py
+++
@@ -1,7 +0,0 @@
-numbers = list(map(int , input().split()))
-
-while numbers.count(0) != 3:
-    numbers.sort(reverse=True)
-    print("right") if numbers[0] ** 2 == numbers[1] ** 2 + numbers[2] ** 2 else print("wrong")
-    
-    numbers = list(map(int , input().split()))
