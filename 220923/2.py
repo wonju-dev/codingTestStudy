2c6bf62a4911df4bcefce2412ecfaf245aa843d2
wonju-dev <wonjulee.dev@gmail.com>
2022-09-30 17:39:38 +0900

    [220930]차차차 실패(93)

--- 220923/2.py
+++ 220923/2.py
@@ -1,11 +1,38 @@
-# dp
-loop = int(input())
+def getObstacleScore(graph, row, col):
+    score = 0
+    for dy in [-1, 1]:
+        near = col + dy
+        if 0 <= near < 5 and graph[row][near] == 1:
+            score += 1
+    return score
 
-for _ in range(loop):
+
+case = int(input())
+
+while case:
+
     m = int(input())
     graph = []
 
-    for __ in range(m):
+    for _ in range(m):
         graph.append(list(map(int, input().split())))
 
-    dp = [[0] * 5] * m
+    dp = [[0] * 5 for _ in range(m+1)]
+
+    for row in range(m - 1, -1, -1):
+        for col in range(5):
+            if graph[row][col] != 1:
+                if col == 0:
+                    dp[row][col] = max([dp[row+1][col], dp[row+1][col+1]])
+                elif col == 4:
+                    dp[row][col] = max([dp[row+1][col-1], dp[row+1][col]])
+                else:
+                    dp[row][col] = max([dp[row+1][col-1], dp[row+1][col], dp[row+1][col+1]])
+                
+                dp[row][col] += graph[row][col] + getObstacleScore(graph, row, col)
+            else:
+                dp[row][col] = -1
+
+    print(max(dp[0]))
+
+    case -= 1
