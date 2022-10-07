c23476d2fe18586e9644a09b748588a57e54e74f
wonju-dev <wonjulee.dev@gmail.com>
2022-10-07 16:20:00 +0900

    temp

--- 201007/1.py
+++ 201007/1.py
@@ -1,58 +1,9 @@
-from collections import deque
-
-
 case = int(input())
 
 while case > 0:
+
     width, height, destination = map(int, input().split())
+
     width = width * 2 - 1
-
-    graph = []
-
-    for _ in range(height):
-        graph.append(list(input()))
-
-    q = deque()
-    q.append((height - 1, (destination - 1) * 2, (-1, -1)))
-
-    starts = []
-
-    while q:
-        row, col, previous = q.popleft()
-        print(row, col, previous)
-
-        if row == 0:
-            print("finish", row, col, previous)
-            starts.append(str(col / 2 + 1))
-        else:
-            for d in [-1, 1]:
-                nextCol = col + d
-                if 0 <= nextCol < width and (row != previous[0] or nextCol != previous[1]):
-                    if graph[row][nextCol] == "+":
-                        q.append((row, nextCol, (row, col)))
-                        break
-                    elif graph[row][nextCol] == "?":
-                        if notCross(row, col):
-                            q.append((row, nextCol, (row, col)))
-                        if graph[row-1][col] != "0":
-                            q.append((row - 1, col, (row, col)))
-                        break
-            else:
-                q.append((row - 1, col, (row, col)))
-
-    print(" ".join(sorted(starts)))
-
+   
     case -=1
-
-
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-def notCross(row, col):
-    for i in range(4):
-        x = row + dx[i]
-        y = col + dy[i]
-        if 0 <= x < height and 0 <= 0 < width - 1:
-            if graph[x][y] == 0:
-                return False
-
-    return True
--- 220930/1.py
+++ 220930/1.py
@@ -2,17 +2,53 @@ case = int(input())
 
 while case > 0:
 
-    chunks = input().split()
     length = int(input())
-    numbers = []
-    operators = []
+    chunks = input().split()
+
+    opStack = []
+    outStack = []
 
     for chunk in chunks:
-        if chunk in ["+", "-", "*", "(", ")"]:
-            operators.append(chunk)
+        if chunk in ["*", "("]:
+            opStack.append(chunk)
+        elif chunk in ["+", "-"]:
+            if len(opStack) == 0:
+                opStack.append(chunk)
+            else:
+                while opStack[-1] == "*":
+                    outStack.append(opStack.pop())
+                opStack.append(chunk)
+        elif chunk == ")":
+            while opStack[-1] != "(":
+                outStack.append(opStack.pop())
+            opStack.pop()
         else:
-            numbers.append(int(chunk))
+            outStack.append(chunk)
 
+        # print(opStack, outStack)
+
+    while len(opStack) != 0:
+        outStack.append(opStack.pop())
+
+    temp = []
+    for chunk in outStack:
+        if chunk.isdecimal():
+            # print(chunk, type(chunk))
+
+            temp.append(chunk)
+        else:
+            op2 = int(temp.pop())
+            op1 = int(temp.pop())
+
+            if chunk == "+":
+                temp.append(op1 + op2)
+            elif chunk == "-":
+                temp.append(op1 - op2)
+            elif chunk == "*":
+                temp.append(op1 * op2)
+    
+    print(temp[0])
+    
     case -=1
 
 """
