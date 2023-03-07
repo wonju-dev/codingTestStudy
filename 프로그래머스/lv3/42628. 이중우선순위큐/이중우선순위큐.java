120f6e3669c54ed44f0181c2e3bd2c9d8a39b432
WonjuLee <wonjulee.dev@gmail.com>
2023-03-07 13:48:47 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.57 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,52 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution {
+
+    public static PriorityQueue<Integer> q = new PriorityQueue();
+    public static PriorityQueue<Integer> rq = new PriorityQueue<>(Comparator.reverseOrder());
+
+    public int[] solution(String[] operations) {
+        for (String operation : operations) {
+            String[] s = operation.split(" ");
+            String op = s[0];
+            Integer value = Integer.parseInt(s[1]);
+
+            if (op.equals("I")) {
+                insert(value);
+            } else {
+                if (rq.size() < 1) {
+                    continue;
+                }
+                if (value == -1) {
+                    deleteMin();
+                } else {
+                    deleteMax();
+                }
+            }
+        }
+        int[] answer = new int[2];
+        System.out.println(q);
+        System.out.println(rq);
+        if (rq.size() > 0) {
+            answer[0] = rq.poll();
+            answer[1] = q.poll();
+        }
+        return answer;
+    }
+
+    private void deleteMax() {
+        Integer poll = rq.poll();
+        q.remove(poll);
+    }
+
+    private void deleteMin() {
+        Integer poll = q.poll();
+        rq.remove(poll);
+    }
+
+    private void insert(Integer value) {
+        q.add(value);
+        rq.add(value);
+    }
+}
