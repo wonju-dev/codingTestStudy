0e3ee5981b92317282a06087da5fd183dafa55e6
WonjuLee <wonjulee.dev@gmail.com>
2023-03-11 16:36:20 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,13 +1,13 @@
 class Solution {
     public long solution(int n) {
-        long[] memo = new long[2001];
-        memo[1] = 1;
-        memo[2] = 2;
-
+        long[] dp = new long[2001];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
         for (int i = 3; i <= n; i++) {
-            memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567;
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
-
-        return memo[n];
+        return dp[n];
     }
 }
