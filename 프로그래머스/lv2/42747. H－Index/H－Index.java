2cab4531bda94646f63cf90c8c0b74caefb98728
WonjuLee <wonjulee.dev@gmail.com>
2023-03-14 13:27:07 +0900

    [level 2] Title: H-Index, Time: 0.36 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= citations.length - i) {
+                return citations.length - i;
+            }
+        }
+        return 0;
+    }
+}
