c9d3d779e11494c8383fd1d4982ce3e9ac56d1b6
WonjuLee <wonjulee.dev@gmail.com>
2023-03-30 13:22:37 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0 ; i < my_string.length(); i++) {
-            sb.append(my_string.charAt(i));
-        }
+        sb.append(my_string);
         return sb.reverse().toString();
     }
 }
