35b7a4246585eb3db8ffb3e8ecee37ec93b5b480
WonjuLee <wonjulee.dev@gmail.com>
2023-03-31 16:05:58 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0, 0};
+        for (int num : num_list) {
+            if (num % 2 == 0) {
+                answer[0]++;
+            } else {
+                answer[1]++;
+            }
+        }
+        return answer;
+    }
+}
